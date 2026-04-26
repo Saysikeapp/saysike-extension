ebfe88a0011dcd3de691e6b10bd56010207fe591
Joe Hawkins <josephlhawkins2@gmail.com>
2026-04-26 15:56:22 +0100

    (chore) cleanup

--- src/entrypoints/content/index.ts
+++ src/entrypoints/content/index.ts
@@ -1,6 +1,7 @@
 export default defineContentScript({
   matches: ["http://*/*", "https://*/*"],
   main() {
+    // Leaving this as a placeholder for when content scripts are needed in future
     const nonsense = "Currently not in use :)";
   },
 });
