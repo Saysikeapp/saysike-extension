bd437bad047f439b72154d0275571094a4896368
Joe Hawkins <josephlhawkins2@gmail.com>
2026-04-26 21:56:39 +0100

    chore: remove unused device util

--- src/lib/utils/src/index.ts
+++ src/lib/utils/src/index.ts
@@ -1,7 +1,6 @@
 export * from "./assert";
 export * from "./browser";
 export * from "./domain";
-export * from "./device";
 export * from "./domain";
 export * from "./regex";
 export * from "./sleep";
